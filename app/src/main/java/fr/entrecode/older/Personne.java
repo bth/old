dd391fd8d577e662508fc7d96c9bf37f43c1a5f1
bth <contactbth@gmail.com>
2015-04-20 19:54:04 +0200

    Création de la classe Personne.

---
+++ app/src/main/java/fr/entrecode/older/Personne.java
@@ -0,0 +1,30 @@
+package fr.entrecode.older;
+
+/**
+ * Created by ben on 20/04/15.
+ */
+public class Personne {
+
+    private String dateNaissance;
+    private String Nom;
+
+    public String getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(String dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getNom() {
+        return Nom;
+    }
+
+    public void setNom(String nom) {
+        Nom = nom;
+    }
+
+    public int getAge() {
+        return 0;
+    }
+}
