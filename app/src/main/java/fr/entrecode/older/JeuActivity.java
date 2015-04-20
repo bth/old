982d6455caa12fcee40c25d3094f42a50db9dc9f
bth <contactbth@gmail.com>
2015-04-20 22:14:01 +0200

    Calcul de l'age des Personnes.

--- app/src/main/java/fr/entrecode/older/JeuActivity.java
+++ app/src/main/java/fr/entrecode/older/JeuActivity.java
@@ -2,6 +2,7 @@ package fr.entrecode.older;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Created by ben on 19/04/15.
@@ -17,5 +18,14 @@ public class JeuActivity extends Activity {
 
     private void init() {
 
+        // Initialisation de la liste des Personnes
+        Personne p1 = new Personne("Daniel Radcliffe", "23/07/1989");
+        Log.d("debug", String.valueOf(p1.getAge()));
+
+        Personne p2 = new Personne("Daniel Radcliffe", "08/12/1988");
+        Log.d("debug", String.valueOf(p2.getAge()));
+
+        Personne p3 = new Personne("Daniel Radcliffe", "19/04/1988");
+        Log.d("debug", String.valueOf(p3.getAge()));
     }
 }
--- app/src/main/java/fr/entrecode/older/Personne.java
+++ app/src/main/java/fr/entrecode/older/Personne.java
@@ -1,5 +1,9 @@
 package fr.entrecode.older;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  * Created by ben on 20/04/15.
  */
@@ -7,6 +11,11 @@ public class Personne {
 
     private String dateNaissance;
     private String Nom;
+
+    public Personne(String nom, String dateNaissance) {
+        setNom(nom);
+        setDateNaissance(dateNaissance);
+    }
 
     public String getDateNaissance() {
         return dateNaissance;
@@ -25,6 +34,22 @@ public class Personne {
     }
 
     public int getAge() {
-        return 0;
+        String[] parties = getDateNaissance().split("/");
+        int age = 0;
+        if (parties.length == 3) {
+            Calendar naissance = new GregorianCalendar();
+            Calendar aujourdhui = new GregorianCalendar();
+            naissance.set(Integer.parseInt(parties[2]), Integer.parseInt(parties[1])-1, Integer.parseInt(parties[0]));
+
+            int anneeDifference = aujourdhui.get(Calendar.YEAR) - naissance.get(Calendar.YEAR);
+            naissance.set(Calendar.YEAR, aujourdhui.get(Calendar.YEAR));
+            if (!naissance.after(aujourdhui)) {
+                age = anneeDifference;
+            }
+            else {
+                age = anneeDifference - 1;
+            }
+        }
+        return age;
     }
 }
