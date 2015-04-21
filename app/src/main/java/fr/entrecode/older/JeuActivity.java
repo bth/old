afcc2e8db39d80f28b62aa39679c802744de42f1
bth <contactbth@gmail.com>
2015-04-21 22:19:58 +0200

    Affichage des questions.

--- app/src/main/java/fr/entrecode/older/JeuActivity.java
+++ app/src/main/java/fr/entrecode/older/JeuActivity.java
@@ -1,8 +1,11 @@
 package fr.entrecode.older;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,10 +32,30 @@ public class JeuActivity extends Activity {
         Personne p1 = new Personne("a", "Daniel Radcliffe", "23/07/1989");
         Log.d("debug", String.valueOf(p1.getAge()));
 
-        Personne p2 = new Personne("b", "Daniel Radcliffe", "08/12/1988");
+        Personne p2 = new Personne("a", "Daniel Radcliffe", "08/12/1988");
         Log.d("debug", String.valueOf(p2.getAge()));
 
         Personne p3 = new Personne("c", "Daniel Radcliffe", "19/04/1988");
         Log.d("debug", String.valueOf(p3.getAge()));
+
+        afficherQuestion(p1, p2);
+    }
+
+    private void afficherQuestion(Personne personneA, Personne personneB) {
+        Resources r = getResources();
+
+        // Affichage de la Personne A
+        ImageView photoA = (ImageView)findViewById(R.id.photoA);
+        photoA.setImageDrawable(r.getDrawable(r.getIdentifier(personneA.getId(), "drawable", "fr.entrecode.older")));
+
+        TextView nomA = (TextView)findViewById(R.id.nomA);
+        nomA.setText(personneA.getNom());
+
+        // Affichage de la Personne B
+        ImageView photoB = (ImageView)findViewById(R.id.photoB);
+        photoB.setImageDrawable(r.getDrawable(r.getIdentifier(personneB.getId(), "drawable", "fr.entrecode.older")));
+
+        TextView nomB = (TextView)findViewById(R.id.nomB);
+        nomB.setText(personneB.getNom());
     }
 }
